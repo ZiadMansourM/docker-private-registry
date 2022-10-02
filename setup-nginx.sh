da94eb710e217f0fbd5bde977c344fd0bffd01a2
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-10-03 00:55:18 +0200

    ➕ ADD: Authentication

---
+++ setup-nginx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Install Nginx
+sudo apt update
+sudo apt install nginx --yes
+# sudo ufw app list
+# sudo ufw allow 'Nginx HTTP'
+# sudo ufw status
+# systemctl status nginx
+
+# sudo systemctl stop nginx
+# sudo systemctl start nginx
+# sudo systemctl restart nginx
+# sudo systemctl reload nginx
+# sudo systemctl disable nginx
+# sudo systemctl enable nginx
+
+
+# Install certbot
+# sudo apt install certbot python3-certbot-nginx --yes
+# sudo certbot --nginx -d example.com -d www.example.com
