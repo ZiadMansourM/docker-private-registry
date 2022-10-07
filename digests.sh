9c1e64ad14ac3bafee42b4401c7bf320042310db
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2022-10-07 14:27:48 +0200

    ➕ ADD: Refactor

--- digests.sh
+++ digests.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+HOST=registry.sreboy.com
+USER=docker
+PASSWORD=silver
+
 REPOS=$(\
-    curl -sS --user docker:silver  https://registry.sreboy.com/v2/_catalog \
+    curl -sS --user $USER:$PASSWORD  https://$HOST/v2/_catalog \
     | python3 -c "import sys, json; data=json.load(sys.stdin)['repositories']; [print(repo) for repo in data]"\
 )
 
 for REPO in $REPOS
 do
     TAGS=$(\
-        curl -sS --user docker:silver  https://registry.sreboy.com/v2/$REPO/tags/list \
+        curl -sS --user $USER:$PASSWORD  https://$HOST/v2/$REPO/tags/list \
         | python3 -c "import sys, json; data=json.load(sys.stdin)['tags']; [print(tag) for tag in data]"\
     )
     for TAG in $TAGS
     do
         DIGIST=$(\
-            curl -sS --user docker:silver \
+            curl -sS --user $USER:$PASSWORD \
             -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' \
-            https://registry.sreboy.com/v2/$REPO/manifests/$TAG \
+            https://$HOST/v2/$REPO/manifests/$TAG \
             | python3 -c "import sys, json; print(json.load(sys.stdin)['config']['digest'])"\
         )
         echo $REPO:$TAG:$DIGIST
